b6c983c619451ca1c46fce662acd4499479e9da7
Robin <robinkolk@msn.com>
2018-05-03 19:48:02 +0200

    more logging :(

--- host/src/main/java/org/area515/resinprinter/network/LinuxNetworkManager.java
+++ host/src/main/java/org/area515/resinprinter/network/LinuxNetworkManager.java
@@ -98,11 +98,13 @@ public class LinuxNetworkManager implements NetworkManager {
 	public List<NetInterface> getNetworkInterfaces() {
 		List<NetInterface> ifaces = new ArrayList<NetInterface>();
 		String[] nics = IOUtilities.executeNativeCommand(new String[]{"/bin/sh", "-c", "ifconfig | grep flags | awk '''{ print $1 }'''"}, null);
-		logger.info("nics returned :" + nics);
 		for (String nicName : nics) {
+			logger.info("nicname returned :" + nicName);
+			logger.info("nics returned :" + nics);
 			NetInterface netFace = new NetInterface();
 			netFace.setName(nicName);
 			ifaces.add(netFace);
+			logger.info("ifaces returned :" + ifaces);
 			
 			Boolean doneLookingForWifi = null;
 			while (doneLookingForWifi == null || !doneLookingForWifi) {
